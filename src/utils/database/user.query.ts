07bc4724f76dd960cbdd65dc9fb381a65dc1b792
Deryl Feyza <muhammadderylfeyza@gmail.com>
2024-08-21 08:00:24 +0700

    feat: user database actions

---
+++ src/utils/database/user.query.ts
@@ -0,0 +1,41 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { encrypt } from "@/utils/encrypt";
+import { Prisma } from "@prisma/client";
+
+export const findAllUser = async () => {
+	return await prisma.user.findMany();
+};
+
+export const findUser = async (where: Prisma.UserWhereUniqueInput) => {
+	return await prisma.user.findUnique({ where });
+};
+
+export const createUser = async (data: Prisma.UserCreateInput) => {
+	return await prisma.user.create({
+		data: {
+			...data,
+			password: data.password ? await encrypt(data.password) : undefined,
+		},
+	});
+};
+
+export const updateUser = async (
+	where: Prisma.UserWhereUniqueInput,
+	data: Prisma.UserUpdateInput
+) => {
+	return await prisma.user.update({
+		where,
+		data: {
+			...data,
+			password: data.password
+				? await encrypt(data.password as string)
+				: undefined,
+		},
+	});
+};
+
+export const deleteUser = async (where: Prisma.UserWhereUniqueInput) => {
+	return await prisma.user.delete({ where });
+};
