2be2e0f6d4ffa2c70634c7604b8532f759881359
Deryl Feyza <muhammadderylfeyza@gmail.com>
2024-08-23 20:31:35 +0700

    cloudinary setup

--- global.d.ts
+++ global.d.ts
@@ -6,6 +6,10 @@ declare global {
 	namespace NodeJS {
 		interface ProcessEnv {
 			DATABASE_URL: string;
+			CLOUDINARY_URL: string;
+			NEXTAUTH_SECRET: string;
+			GOOGLE_CLIENT_ID: string;
+			GOOGLE_CLIENT_SECRET: string;
 		}
 	}
 }
