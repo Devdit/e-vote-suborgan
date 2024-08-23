9dcc7f4f47070775309d5f4b903b480918a92f3f
Deryl Feyza <muhammadderylfeyza@gmail.com>
2024-08-23 08:04:23 +0700

    feat: candidate actions

---
+++ src/utils/actions/candidates.ts
@@ -0,0 +1,41 @@
+"use server";
+
+import { createCandidate, updateCandidate } from "../database/candidates.query";
+
+export const handleCreateCandidate = async (formData: FormData) => {
+	const candidateData = {
+		img: formData.get("img") as string,
+		name: formData.get("name") as string,
+		kelas: formData.get("kelas") as string,
+		visi: formData.get("visi") as string,
+		misi: formData.get("misi") as string,
+		pengalaman: formData.get("pengalaman") as string,
+		moto: formData.get("moto") as string,
+		progja: formData.get("progja") as string,
+		video_profil: formData.get("video_profil") as string,
+		suborgan: {
+			connect: { id: formData.get("suborgan_id") as string },
+		},
+	};
+
+	await createCandidate(candidateData);
+};
+
+export const handleUpdateCandidate = async (id: string, formData: FormData) => {
+	const candidateData = {
+		img: formData.get("img") as string,
+		name: formData.get("name") as string,
+		kelas: formData.get("kelas") as string,
+		visi: formData.get("visi") as string,
+		misi: formData.get("misi") as string,
+		pengalaman: formData.get("pengalaman") as string,
+		moto: formData.get("moto") as string,
+		progja: formData.get("progja") as string,
+		video_profil: formData.get("video_profil") as string,
+		suborgan: {
+			connect: { id: formData.get("suborgan_id") as string },
+		},
+	};
+
+	await updateCandidate({ id }, candidateData);
+};
